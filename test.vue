d6d1ce78165040a709c432ed8b618831d8c68ade
markdown456 <68313196+markdown456@users.noreply.github.com>
2025-07-19 17:47:47 +0800

    新增 test.vue 文件

---
+++ test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Test Vue 组件</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
